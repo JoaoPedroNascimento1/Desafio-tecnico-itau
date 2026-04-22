76f0e3f81504a2665a959e472498632ef1e5f20b
Joao Pedro Nascimento <nascjp.dev@gmail.com>
2026-04-22 16:03:21 -0300

    adiciona o metodo estatistica na TransacaoController

--- src/main/java/br/com/desafioItau/repository/TransacaoRepository.java
+++ src/main/java/br/com/desafioItau/repository/TransacaoRepository.java
@@ -3,8 +3,10 @@ package br.com.desafioItau.repository;
 import br.com.desafioItau.dto.mapper.TransacaoMapper;
 import br.com.desafioItau.dto.request.TransacaoRequest;
 import br.com.desafioItau.dto.response.TransacaoResponse;
+import br.com.desafioItau.entity.Estatistica;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +21,18 @@ public class TransacaoRepository {
 
   public void deletarDados(){
     transacoes.clear();
+  }
+
+  public Estatistica estatistica(OffsetDateTime horaInicial){
+    if(transacoes.isEmpty()){
+      return new Estatistica(0,0.0,0.0,0.0,0.0);
+    }
+
+    var resumo = transacoes.stream().filter(t -> t.dataHora().isAfter(horaInicial) || t.dataHora().isEqual(horaInicial))
+      .mapToDouble(t -> t.valor().doubleValue())
+      .summaryStatistics();
+
+    return new Estatistica(resumo.getCount(), resumo.getSum(), resumo.getAverage(), resumo.getMax(),  resumo.getMin());
   }
 
 }
